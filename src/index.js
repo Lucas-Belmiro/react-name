50865e31a1a1d0c8dbde1ac5a68de95ea738cbae
Lucas-Belmiro <lucasrbelmiro@gmail.com>
2023-10-17 19:50:36 -0300

    add component1

--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import PrimeiroComponente from './Components/PrimeiroCompenente/PrimeiroComponente';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <PrimeiroComponente></PrimeiroComponente>
+    
   </React.StrictMode>
 );
 
